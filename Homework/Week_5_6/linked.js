0bc52d30e49e063184ac880a3d423ea3ad00bbeb
Lucas-coder <lucaslumeij@gmail.com>
2018-05-09 15:45:54 +0200

    A working bar chart

--- Homework/Week_5_6/linked.js
+++ Homework/Week_5_6/linked.js
@@ -1,9 +1,7 @@
 window.onload = function() {
 
-    // console.log("It works, motherfuckers!");
-
-    var countries = "http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/hlth_cd_aro?sex=T&geo=AT&geo=BE&geo=BG&geo=CH&geo=CY&geo=CZ&geo=DE&geo=DK&geo=EE&geo=EL&geo=ES&geo=FI&geo=FR&geo=HR&geo=HU&geo=IE&geo=IS&geo=IT&geo=LI&geo=LT&geo=LU&geo=LV&geo=MT&geo=NL&geo=NO&geo=PL&geo=PT&geo=RO&geo=RS&geo=SE&geo=SI&geo=SK&geo=TR&geo=UK&resid=TOT_RESID&precision=1&unit=NR&time=2015&age=TOTAL&icd10=G_H";
-    var diseases = "http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/hlth_cd_aro?sex=T&geo=AT&geo=BE&geo=BG&geo=CH&geo=CY&geo=CZ&geo=DE&geo=DK&geo=EE&geo=EL&geo=ES&geo=FI&geo=FR&geo=HR&geo=HU&geo=IE&geo=IS&geo=IT&geo=LI&geo=LT&geo=LU&geo=LV&geo=MT&geo=NL&geo=NO&geo=PL&geo=PT&geo=RO&geo=RS&geo=SE&geo=SI&geo=SK&geo=TR&geo=UK&resid=TOT_RESID&precision=1&unit=NR&time=2015&age=TOTAL&icd10=G20&icd10=G30&icd10=G_H&icd10=G_H_OTH";
+    var countries = "http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/hlth_cd_aro?sex=T&geo=AT&geo=BE&geo=BG&geo=CH&geo=CY&geo=CZ&geo=DE&geo=DK&geo=EE&geo=EL&geo=ES&geo=FI&geo=FR&geo=HR&geo=HU&geo=IE&geo=IS&geo=IT&geo=LI&geo=LT&geo=LU&geo=LV&geo=MT&geo=NL&geo=NO&geo=PL&geo=PT&geo=RO&geo=RS&geo=SE&geo=SI&geo=SK&geo=TR&geo=UK&geo=EU28&resid=TOT_RESID&precision=1&unit=NR&time=2015&age=TOTAL&icd10=G_H";
+    var diseases = "http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/hlth_cd_aro?sex=T&geo=AT&geo=BE&geo=BG&geo=CH&geo=CY&geo=CZ&geo=DE&geo=DK&geo=EE&geo=EL&geo=ES&geo=FI&geo=FR&geo=HR&geo=HU&geo=IE&geo=IS&geo=IT&geo=LI&geo=LT&geo=LU&geo=LV&geo=MT&geo=NL&geo=NO&geo=PL&geo=PT&geo=RO&geo=RS&geo=SE&geo=SI&geo=SK&geo=TR&geo=UK&geo=EU28&resid=TOT_RESID&precision=1&unit=NR&time=2015&age=TOTAL&icd10=G20&icd10=G30&icd10=G_H&icd10=G_H_OTH";
 
     d3.queue()
       .defer(d3.request, countries)
@@ -14,14 +12,19 @@ window.onload = function() {
           var perCountry = JSON.parse(result[0].responseText);
           var perDisease = JSON.parse(result[1].responseText);
 
-          pagePrep(perCountry, perDisease);
+          drawBarchart(perDisease);
       });
 };
 
-function pagePrep(perCountry, perDisease) {
+function drawBarchart(perDisease) {
 
     var fullWidthChart = 800;
     var fullHeightChart = 600;
+
+    var margin = {top: 10, right: 75, bottom: 125, left: 100};
+
+    var width = fullWidthChart - margin.left - margin.right;
+    var height = fullHeightChart - margin.top - margin.bottom;
 
     var svgChart = d3.select("body")
                      .append("svg")
@@ -29,9 +32,62 @@ function pagePrep(perCountry, perDisease) {
                      .attr("width", fullWidthChart)
                      .attr("height", fullHeightChart);
 
-    // drawBarchart(perDisease);
+    var barData = convertData(perDisease, 2);
 
-    convertData(perCountry, perDisease);
+    var country = barData["European Union"];
+
+    console.log(barData);
+
+    var xScale = d3.scaleBand()
+                   .domain((Object.keys(country)).map(function(d) { return d; }))
+                   .range([margin.left, width + margin.left])
+                   .padding(0.1);
+
+    var deathsMax = d3.max(Object.values(country), function(d) { return d });
+
+    var yScale = d3.scaleLinear()
+                   .domain([0, deathsMax])
+                   .range([height + margin.top, margin.top]);
+
+    // create x axis variable and put at bottom
+    var xAxis = d3.axisBottom(xScale);
+
+    // create y axis variable and put at left
+    var yAxis = d3.axisLeft(yScale);
+
+    svgChart.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + (height + margin.top) + ")")
+            .call(xAxis);
+
+    svgChart.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + margin.left + ",0)")
+            .call(yAxis);
+
+    // create all rectangles that function as bars
+    svgChart.selectAll("rect")
+            .data(Object.keys(country))
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+
+            // determine coordinates where to start drawing
+            .attr("x", function(d) {
+              // console.log(d, country[d]);
+                return xScale(d);
+            })
+            .attr("y", function(d) {
+                return yScale(country[d]);
+            })
+
+            // determine dimensions of each rectangle
+            .attr("width", function(d) {
+                return xScale.bandwidth();
+            })
+            .attr("height", function(d) {
+                return height + margin.top - yScale(country[d])
+            });
 };
 
 // function drawMap(perCountry) {
@@ -40,40 +96,44 @@ function pagePrep(perCountry, perDisease) {
 //
 // function drawBarchart(perDisease) {
 //
-//     var xScale =
+//
 // };
 
-function convertData(perCountry, perDisease) {
 
-    perDisease.dimension.geo.category.label.DE = "Germany";
+function convertData(dataset, choice) {
 
-    console.log(perCountry);
+    // give two labels shorter names
+    dataset.dimension.geo.category.label.DE = "Germany";
+    dataset.dimension.geo.category.label.EU28 = "European Union";
+
+    // console.log(perCountry);
 
     var countryList = [];
-    for (let key in perDisease.dimension.geo.category.label) {
-        var name = perDisease.dimension.geo.category.label[key];
+    for (let key in dataset.dimension.geo.category.label) {
+        var name = dataset.dimension.geo.category.label[key];
         countryList.push(name);
     };
 
-    var mapData = {};
-    for (let i = 0; i < perCountry.size[5]; i++) {
-        var object = mapData[countryList[i]] = {};
-        object.total = perCountry.value[i];
-    };
-    console.log(mapData);
-    console.log(Object.keys(mapData));
+    if (choice == 1) {
+        var data = {};
+        for (let i = 0; i < dataset.size[5]; i++) {
+            var object = data[countryList[i]] = {};
+            object.total = dataset.value[i];
+        };
+        // console.log(data);
+        // console.log(Object.keys(mapData));
+    }
 
-    var pieData = {};
-    for (let i = 0; i < perDisease.size[5]; i++) {
-        var object = pieData[countryList[i]] = {};
-        object.PD = perDisease.value[i];
-        object.AD = perDisease.value[i + perDisease.size[5]];
-        object.other = perDisease.value[i + 3 * perDisease.size[5]];
-        object.total = perDisease.value[i + 2 * perDisease.size[5]];
+    else if (choice == 2) {
+        var data = {};
+        for (let i = 0; i < dataset.size[5]; i++) {
+            var object = data[countryList[i]] = {};
+            object.Parkinson = dataset.value[i];
+            object.Alzheimer = dataset.value[i + dataset.size[5]];
+            object.Other = dataset.value[i + 3 * dataset.size[5]];
+            object.Total = dataset.value[i + 2 * dataset.size[5]];
+        };
     };
-    // console.log(pieData);
-    //
-    // console.log(Object.keys(pieData));
-    // console.log(Object.keys(pieData.Austria));
-    // return pieData;
+
+    return data;
 };
